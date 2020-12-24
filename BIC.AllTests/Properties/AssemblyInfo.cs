e65461cb24fedd22f4ad2cb8b3143eb8a503abce
Stan K <stan.kossovskiy@gmail.com>
2020-12-23 22:34:53 -0500

    Add Test project

---
+++ BIC.AllTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BIC.AllTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BIC.AllTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fa77668d-4ef0-4d78-99a4-b21d726f6bd6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BIC.AllTests/TEST_Extract.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BIC.AllTests
+{
+    [TestClass]
+    public class TEST_Extract
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
