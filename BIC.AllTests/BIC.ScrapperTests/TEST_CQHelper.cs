eb57a25b4b37ba55e5e77221c206e8d369736661
Stan K <stan.kossovskiy@gmail.com>
2020-12-29 08:17:24 -0500

    added some tests, fixed logging without parameters

--- BIC.AllTests/BIC.ScrapperTests/TEST_CQHelper.cs
+++ BIC.AllTests/BIC.ScrapperTests/TEST_CQHelper.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BIC.Scrappers.Utils;
+using BIC.Scrappers.FinvizScrapper;
+using BIC.Utils.Logger;
 
 namespace BIC.ScrapperTests
 {
     [TestClass]
     public class TEST_CQHelper
     {
+        private ILog _logger = LogServiceProvider.Logger;
+
         [TestMethod]
         public void TestCq()
         {
@@ -20,6 +24,37 @@ namespace BIC.ScrapperTests
             var cq = cqHelper.GetData("https://finviz.com/screener.ashx");
             Assert.IsTrue(cq.Elements.Count() > 0, "Returns no elements");
         }
+        [TestMethod]
+        public void TestCqWithParameters()
+        {
+            Assert.IsTrue(UtilsForTesting.SetTheSettings(), "Settings initialization failed");
 
+            var r = new HttpRequestData();
+            r.View = "111";
+            r.FilterView = "4";
+            r.Filters = new HttpRequestData.Filter() { Sector = "basicmaterials", Industry = "gold", Country = "usa" };
+            var generatedAddress = r.GenerateAddressRequest();
+
+            var cqHelper = new CQHelper();
+            var cq = cqHelper.GetData(generatedAddress);
+            Assert.IsTrue(cq.Elements.Count() > 0, "Returns no elements");
+        }
+        [TestMethod]
+        public void TestCqWithParametersFindTable()
+        {
+            Assert.IsTrue(UtilsForTesting.SetTheSettings(), "Settings initialization failed");
+
+            var r = new HttpRequestData();
+            r.View = "111";
+            r.FilterView = "4";
+            r.Filters = new HttpRequestData.Filter() { Sector = "basicmaterials", Industry = "gold", Country = "usa" };
+            var generatedAddress = r.GenerateAddressRequest();
+
+            var cqHelper = new CQHelper();
+            var cq = cqHelper.GetData(generatedAddress);
+            Assert.IsTrue(cq.Elements.Count() > 0, "Returns no elements");
+
+            _logger.Debug(cq.Find(@"table[bgcolor=""#d3d3d3""]").Contents().Text());
+        }
     }
 }
