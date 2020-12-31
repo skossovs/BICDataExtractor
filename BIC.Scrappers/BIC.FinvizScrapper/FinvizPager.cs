321c6006ed5f2ab553cfbd9e900ce3b82b17ae0e
Stan K <stan.kossovskiy@gmail.com>
2020-12-31 00:06:08 -0500

    Add data structures

---
+++ BIC.Scrappers/BIC.FinvizScrapper/FinvizPager.cs
@@ -0,0 +1,25 @@
+﻿using BIC.Foundation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIC.Scrappers.FinvizScrapper
+{
+    public class FinvizPager : IPager
+    {
+        public readonly string UrlRoot = Settings.GetInstance().UrlRoot;
+
+        public bool DefineMetrics(out int recordsPerPage, out int maxPage)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GenerateRequestAdresses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
