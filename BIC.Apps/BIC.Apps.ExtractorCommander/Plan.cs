6e69c8c24441dc42843f83f7b1e99929a5789096
skossovs <stan.kossovskiy@gmail.com>
2021-03-12 22:03:31 -0500

    new plans

--- BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
+++ BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
@@ -9,10 +9,14 @@ namespace BIC.Scrappers.FinvizScrapper
     // This class is for planning only doesn't do anything, just thoughts in here
     class Plan
     {
-        // 1. Fix Chrome memory leak. Cut off chrome.exe process mad respawn
+        // 1. Fix Chrome memory leak. Cut off chrome.exe process mad respawn. DONE
         // 2. Implement file logging.
         // 3. Block Chrome logs if possible.
         // 4. Create DB ETL project.
+        // 5. Implement variable timedelay.
+        // 5.1. Implement delay skip if different site detected. So Yahoo doesn't wait for Finviz.
+        // 6. Create Yahoo.Finance Scrapper library
+        // 7. Reorganize tests.
 
     }
 }
