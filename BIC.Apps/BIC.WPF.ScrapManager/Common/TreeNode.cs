a504b03b0032fb1313a8f1fb74255ed12f77d2d4
skossovs <stan.kossovskiy@gmail.com>
2021-04-19 13:30:29 -0400

    Added Galasoft package, ViewModelLocator created - need to use it properly

---
+++ BIC.Apps/BIC.WPF.ScrapManager/Common/TreeNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIC.WPF.ScrapManager.Common
+{
+    public enum TreeNodeType { Invalid = -1, Root = 0,  Command = 1, Parameter = 2 };
+    public class TreeNode
+    {
+        public int Key { get; set; }
+        public TreeNodeType Type { get; set; }
+        public Dictionary<string, string> Properties { get; set; }
+
+    }
+}
