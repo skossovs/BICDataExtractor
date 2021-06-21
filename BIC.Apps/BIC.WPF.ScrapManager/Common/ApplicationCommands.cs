aafa70c75b9a82e00e3bc766095f0bd33e00aff0
skossovs <stan.kossovskiy@gmail.com>
2021-06-20 22:39:03 -0400

    TODO drops

--- BIC.Apps/BIC.WPF.ScrapManager/Common/ApplicationCommands.cs
+++ BIC.Apps/BIC.WPF.ScrapManager/Common/ApplicationCommands.cs
@@ -20,29 +20,7 @@ namespace BIC.WPF.ScrapManager.Common
         {
             CommandCache objectFromYaml = new CommandCache();
             objectFromYaml.ReadCommandYaml("CommandFile.yaml");
-            //using (StreamReader rd = new StreamReader("CommandFile.yaml"))
-            //{
-
-            //    objectFromYaml = Utils.YamlOperations<CommandCache>.ReadObjectFromStream(rd);
-            //}
-
             return objectFromYaml;
         }
-
-        // TODO: Drop it
-        //public static Object ReadJsonObject(string fullPath, Type t)
-        //{
-        //    JsonSerializer serializer = new JsonSerializer();
-        //    serializer.NullValueHandling = NullValueHandling.Include;
-        //    Object result = null;
-
-        //    using (var sr = new StreamReader(fullPath))
-        //    using (var reader = new JsonTextReader(sr))
-        //    {
-        //        JObject jObject = (JObject)serializer.Deserialize(reader);
-        //        result = jObject.ToObject(t);
-        //    }
-        //    return result;
-        //}
     }
 }
