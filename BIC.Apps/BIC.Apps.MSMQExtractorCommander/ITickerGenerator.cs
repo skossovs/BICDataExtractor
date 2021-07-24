53d93a25fccb5dc8231c93171aa919af9a851f8e
skossovs <stan.kossovskiy@gmail.com>
2021-07-24 16:17:13 -0400

    added interfaces

---
+++ BIC.Apps/BIC.Apps.MSMQExtractorCommander/ITickerGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIC.Apps.MSMQExtractorCommander
+{
+    interface ITickerGenerator
+    {
+        IEnumerable<string> GetTickers();
+    }
+}
---
+++ BIC.Apps/BIC.Apps.MSMQExtractorCommander/TickersBySector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIC.Apps.MSMQExtractorCommander
+{
+    public class TickersBySector : ITickerGenerator
+    {
+        public IEnumerable<string> GetTickers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
