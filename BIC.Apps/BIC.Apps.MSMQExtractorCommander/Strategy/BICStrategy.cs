d8cd51c8487a0b23df3b44c60bb8ab19ef7526da
skossovs <stan.kossovskiy@gmail.com>
2021-08-22 22:39:42 -0400

    implemented all strategies

--- BIC.Apps/BIC.Apps.MSMQExtractorCommander/Strategy/BICStrategy.cs
+++ BIC.Apps/BIC.Apps.MSMQExtractorCommander/Strategy/BICStrategy.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BIC.Foundation.DataObjects;
+using BIC.Scrappers.YahooScrapper;
+using BIC.Utils.Logger;
+using BIC.YahooScrapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,14 +13,20 @@ namespace BIC.Apps.MSMQExtractorCommander.Strategy
     public class BICStrategy : IStrategy
     {
         private StrategyParameters _strategyParameters;
+        private YahooBridgeComponents _yahooComponent;
+        private static ILog _logger = LogServiceProvider.Logger;
+
         public BICStrategy(StrategyParameters strategyParameters)
         {
             _strategyParameters = strategyParameters;
+            _yahooComponent = new YahooBridgeComponents(strategyParameters.Sector, strategyParameters.TickerAt);
         }
 
         public void Execute()
         {
-            throw new NotImplementedException();
+            _logger.Info($"Start Loading BIC Data...");
+            _yahooComponent.Scrap();
+            _logger.Info($"Finished Loading BIC Data.");
         }
     }
 }
--- BIC.Apps/BIC.Apps.MSMQExtractorCommander/YahooBridgeComponents.cs
+++ BIC.Apps/BIC.Apps.MSMQExtractorCommander/YahooBridgeComponents.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BIC.Foundation.DataObjects;
+using BIC.Scrappers.YahooScrapper;
+using BIC.Utils.Logger;
+using BIC.YahooScrapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +10,57 @@ using System.Threading.Tasks;
 
 namespace BIC.Apps.MSMQExtractorCommander
 {
-    class YahooBridgeComponents : IBridgeComponents
+    public class YahooBridgeComponents //: IBridgeComponents
     {
-        public void Scrap<T>() where T : class, new()
+        private ILog _logger = LogServiceProvider.Logger;
+        private string _sector;
+        private string _tickerAfter;
+
+        public YahooBridgeComponents(string sector, string tickerAfter)
         {
-            throw new NotImplementedException();
+            _sector = sector;
+            _tickerAfter = tickerAfter;
+        }
+
+        private IEnumerable<SecurityRecord> GenerateDataFilter()
+        {
+            bool skipTheStep = true;
+
+            Func<string, IEnumerable<SectorRecord>> getSectorsOrSector = (s) =>
+            {
+                if (_sector != null && _sector != string.Empty)
+                    return ETL.SqlServer.DataLayer.SecurityReader.GetSectors().Where(ss => ss.Sector == s);
+                else
+                    return ETL.SqlServer.DataLayer.SecurityReader.GetSectors();
+            };
+
+            foreach (var sector in getSectorsOrSector(_sector))
+            {
+                foreach (var security in ETL.SqlServer.DataLayer.SecurityReader.GetSecurities(sector.Sector))
+                {
+                    if (_tickerAfter != null && security.Ticker == _tickerAfter)
+                    {
+                        skipTheStep = false;
+                        continue;
+                    }
+
+                    if (_tickerAfter != null && skipTheStep)
+                        continue;
+                    yield return security;
+                }
+            }
+        }
+
+        public void Scrap()
+        {
+            foreach(var security in GenerateDataFilter())
+            {
+                var oneShot = new OneShotScrapper();
+                var yp = new YahooParameters() { Ticker = security.Ticker, ReportType = "balance-sheet" }; // It doesn't matter whether it is balance sheet, income or cashflow.
+                _logger.Info($"Loading Ticker {security.Ticker} for report type {yp.ReportType} ..");
+                bool result = oneShot.Scrap(yp);
+                _logger.Info($"Finished Loading Ticker {security.Ticker} for report type {yp.ReportType}.");
+            }
         }
     }
 }
