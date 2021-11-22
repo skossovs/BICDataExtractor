f3912a01d63048054e2e46584dfd0a82dfb5a919
skossovs <stan.kossovskiy@gmail.com>
2021-11-21 19:09:44 -0500

    Added radial wave component. The control itself works, but can't set the property in a trigger.

---
+++ BIC.Apps/BIC.WPF.ScrapManager/Converters/StatusToModeConverter.cs
@@ -0,0 +1,30 @@
+﻿using BIC.Foundation.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BIC.WPF.ScrapManager.Converters
+{
+    public class StatusToModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var eStatus = (EProcessStatus)value;
+
+            if (eStatus == EProcessStatus.Running)
+                return "1";
+            else
+                return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
