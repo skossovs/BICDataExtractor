256e93df031d283c70e4666ad90d039188eca48a
skossovs <stan.kossovskiy@gmail.com>
2022-07-17 17:48:30 -0400

    Plan update. Google Chrome hassle fix.

--- BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
+++ BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
@@ -9,21 +9,19 @@ namespace BIC.Scrappers.FinvizScrapper
     // This class is for planning only doesn't do anything, just thoughts in here
     class Plan
     {
-        // 1.     Create WPF UI
-        // 1.1.   Make stoppable yahoo. DONE
-        // 1.2.   Design is still ugly.
-        // 1.3.   Make Yahoo Feed stoppable. DONE
-        // 1.4.   Expand tree view dynamically.
+        // 1.     Use postman to update google chrome.exe component. Instead of dragging crome.exe with every project REFER TO IT:
+        //        C:\Program Files (x86)\Google\Chrome\Application
+        // 2.     Create WPF UI
+        // 2.1.   Design is still ugly.
+        // 2.2.   Expand tree view dynamically.
 
         // 2.     Reorganize tests.
+
         // 3.     Errors: >>
-        // 3.1.   Merge fails on duplicates. The reason is companies late filling. Need to fix quarters. DONE.
         // 3.2.   Queue doesn't have enough time to process last item, before finished.
         // 3.3.   Before apply FX Rates search for "Currency in CNY.All numbers in thousands" string. Sometimes foreign companies post fundamentals in dollars.
         // 3.4.   fx loading shows no progress.
         // 3.5.   in WPF & MSMQ exe statuses are not properly signalled.
-        // 3.6.   stoppable ETL fails to zip files and breaking the archive. DONE
-        // 4.     Implement Report for yearly data. DONE
         // 5.     Fix linq2db issue with connection object. Specifically for Merge. Looks like Merge operation happens after connection object is closed.
         // 6.     Block Chrome logs if possible.
     }
