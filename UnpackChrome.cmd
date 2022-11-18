64e667f9274ce5a14b0ed9317b09206fff309cdb
skossovs <stan.kossovskiy@gmail.com>
2022-11-17 20:26:52 -0500

    introduced special script that take care of copying

---
+++ UnpackChrome.cmd
@@ -0,0 +1,36 @@
+cd BIC.Apps\BIC.Apps.ExtractorCommander\bin\Debug
+del chromedriver.exe
+tar -xf chromedriver.zip
+del chromedriver.zip
+cd ..\..\..\..\
+
+cd BIC.Apps\BIC.Apps.ExtractorCommander\bin\Release
+del chromedriver.exe
+tar -xf chromedriver.zip
+del chromedriver.zip
+cd ..\..\..\..\
+
+cd BIC.Apps\BIC.Apps.MSMQExtractorCommander\bin\Debug
+del chromedriver.exe
+tar -xf chromedriver.zip
+del chromedriver.zip
+cd ..\..\..\..\
+
+cd BIC.Apps\BIC.Apps.MSMQExtractorCommander\bin\Release
+del chromedriver.exe
+tar -xf chromedriver.zip
+del chromedriver.zip
+cd ..\..\..\..\
+
+cd BIC.AllTests\BIC.ScrapperTests\bin\Debug
+del chromedriver.exe
+tar -xf chromedriver.zip
+del chromedriver.zip
+cd ..\..\..\..\
+
+cd BIC.AllTests\BIC.ScrapperTests\bin\Release
+del chromedriver.exe
+tar -xf chromedriver.zip
+del chromedriver.zip
+
+pause
