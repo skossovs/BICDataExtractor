68ed18bd465bf86ab3169986154f296e955d1061
skossovs <stan.kossovskiy@gmail.com>
2022-12-11 15:25:32 -0500

    Plan update

--- BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
+++ BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
@@ -10,20 +10,21 @@ namespace BIC.Scrappers.FinvizScrapper
     class Plan
     {
         // 1.     Use postman to update google chrome.exe component. Instead of dragging crome.exe with every project REFER TO IT:
-        //        C:\Program Files (x86)\Google\Chrome\Application
+        //        C:\Program Files (x86)\Google\Chrome\Application. DISMISSED.
         // 2.     Create WPF UI
         // 2.1.   Design is still ugly.
         // 2.2.   Expand tree view dynamically.
         // 2.     Reorganize tests.
 
         // 3.     Errors: >>
-        // 3.2.   Queue doesn't have enough time to process last item, before finished.
+        // 3.2.   Queue doesn't have enough time to process last item, before finished. DONE.
         // 3.3.   Before apply FX Rates search for "Currency in CNY.All numbers in thousands" string. Sometimes foreign companies post fundamentals in dollars.
         // 3.4.   fx loading shows no progress.
-        // 3.5.   in WPF & MSMQ exe statuses are not properly signalled.
+        // 3.5.   in WPF & MSMQ exe statuses are not properly signalled when error happened the running status changes.
         // 5.     Fix linq2db issue with connection object. Specifically for Merge. Looks like Merge operation happens after connection object is closed.
-        // 6.     Block Chrome logs if possible.
-        // 7.     Switch to httpClient with separate apartments
+        // 6.     Block Chrome logs if possible. DISMISSED.
+        // 7.1.   Switch to httpClient with separate apartments. DONE.
+        // 7.2.   FX is not working with httpClient 4.6.1 version is needed for .NET >>
         // 8.     Dowload Watched options
         // 8.1.   Implement UI tab.
         // 8.2.   Implement Option feed from yahoo
