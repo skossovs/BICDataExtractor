1fabd5fe250aa395e714e66e7adde0d7d415c02d
skossovs <stan.kossovskiy@gmail.com>
2022-12-11 17:18:21 -0500

    Plan update

--- BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
+++ BIC.Apps/BIC.Apps.ExtractorCommander/Plan.cs
@@ -9,8 +9,6 @@ namespace BIC.Scrappers.FinvizScrapper
     // This class is for planning only doesn't do anything, just thoughts in here
     class Plan
     {
-        // 1.     Use postman to update google chrome.exe component. Instead of dragging crome.exe with every project REFER TO IT:
-        //        C:\Program Files (x86)\Google\Chrome\Application. DISMISSED.
         // 2.     Create WPF UI
         // 2.1.   Design is still ugly.
         // 2.2.   Expand tree view dynamically.
@@ -18,11 +16,9 @@ namespace BIC.Scrappers.FinvizScrapper
 
         // 3.     Errors: >>
         // 3.3.   Before apply FX Rates search for "Currency in CNY.All numbers in thousands" string. Sometimes foreign companies post fundamentals in dollars.
-        // 3.4.   fx loading shows no progress.
+        // 3.4.   fx loading shows no progress. DISMISSED.
         // 3.5.   in WPF & MSMQ exe statuses are not properly signalled when error happened the running status changes.
-        // 5.     Fix linq2db issue with connection object. Specifically for Merge. Looks like Merge operation happens after connection object is closed.
-        // 6.     Block Chrome logs if possible. DISMISSED.
-        // 7.2.   FX is not working with httpClient 4.6.1 version is needed for .NET.  DONE.
+        // 5.     Fix linq2db issue with connection object. Specifically for Merge. Looks like Merge operation happens after connection object is closed. DONE.
         // 8.     Dowload Watched options
         // 8.1.   Implement UI tab.
         // 8.2.   Implement Option feed from yahoo
